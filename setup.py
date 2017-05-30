64ba414df7bb84830d16ef352773e6eefa64b9ad
dciangot <dciangot@cern.ch>
2017-05-30 11:38:35 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MachineLearnig_wrappers',
+    version='v0.1',
+    packages=['TMVA', 'Keras', 'TensorFlow'],
+    url='',
+    license='GNU',
+    author='dciangot',
+    author_email='',
+    description=''
+)
